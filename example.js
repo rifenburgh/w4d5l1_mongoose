1b841a7077d6508d87287e9f677f0783e2b49608
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-02-24 11:14:48 -0500

    Add Schema

--- example.js
+++ example.js
@@ -3,12 +3,21 @@ const mongoose = require('mongoose');
 //this line only needs to run once to connect the application to the database
 mongoose.connect('mongodb://localhost/W4D5L1_Mongoose');
 
+//Create a Schema to keep data consistent in database documents
+//Data types: String, Number, Date, Boolean, Array, Mixed, Objectid, etc.
+//Date is Year Month Day
+const catSchema = new Schema({
+  name: String,
+  color: String,
+  age: {type: Number, min: 0, max: 99, default: 21}
+});
+
 //this line creates the initial structure of the database
 //Mongoose allow this variable to be used as a constructor function that interacts with the 'Cats' collection
 //First object 'Cat' is the collection
 //Second object "{ name: String }is known as the schema
 //mongo will pluralize the model collection name cat ==> cats
-const Cat = mongoose.model('Cat', { name: String });
+const Cat = mongoose.model('Cat', catSchema);
 
 //Creates a new object 'kitty' with details that fit the schema
 //this object has not been persisted to the database, yet
@@ -30,5 +39,5 @@ Cat.find( (err, results) => {
     console.log(`----> ${dog.name}`);
   });
 });
-//Closes database collection after 1 second 
+//Closes database collection after 1 second
 setTimeout(() => mongoose.disconnect(), 1000);
